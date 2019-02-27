7b1ab9e4f8023321a457d1f8b5fcefe5c0f8e6f2
yinyinghuang <934646546@qq.com>
2019-02-27 10:09:03 +0800

    debug fan entity openid cant be saved

--- plugins/Api/src/Controller/FansController.php
+++ plugins/Api/src/Controller/FansController.php
@@ -3,6 +3,7 @@ namespace Api\Controller;
 
 use Api\Controller\AppController;
 use Cake\Http\Client;
+
 /**
  * Fans Controller
  *
@@ -11,55 +12,62 @@ use Cake\Http\Client;
  */
 class FansController extends AppController
 {
-    protected $appid = 'wx594d39c9d198444b';
+    protected $appid  = 'wx594d39c9d198444b';
     protected $secret = '3896265393bd937b00683664282f01f8';
 
     public function login()
     {
         $code = $this->request->getData('code');
-        empty($code) && $this->ret(1,'','缺少code');
+        empty($code) && $this->ret(1, '', '缺少code');
 
         $this->sessionKey = $this->getSessionKey($code);
-        if(array_key_exists('errcode',$this->sessionKey->json)){
-            $this->ret(2,'',$this->sessionKey->json['errmsg']); 
-        }else{
-            $openid = $this->sessionKey->json['openid'];  
-            $fan = $this->Fans->find()->where(['openid' => $openid])->first();
+        if (array_key_exists('errcode', $this->sessionKey->json)) {
+            $this->ret(2, '', $this->sessionKey->json['errmsg']);
+        } else {
+            $openid = $this->sessionKey->json['openid'];
+            $fan    = $this->Fans->find()->where(['openid' => $openid])->first();
 
-            if($fan){
-                $this->ret(0,$fan->id,'登陆成功'); 
-            }else{
-                $fan = $this->Fans->newEntity();
+            if ($fan) {
+                $this->ret(0, $fan->id, '登陆成功');
+            } else {
+                $fan         = $this->Fans->newEntity();
                 $fan->openid = $openid;
-                $params = json_decode($this->request->getData('user_msg_str'),true);
-                $fan = $this->Fans->patchEntity($fan,$params);
+                $params      = json_decode($this->request->getData('user_msg_str'), true);
+                $fan         = $this->Fans->patchEntity($fan, $params);
                 // $schema = $this->Fans->getSchema();
-                // $this->ret(1,$fan->extract($schema->columns(), true),'注册成功'); 
-                $this->ret(5,$this->Fans->checkRules($fan,'create'),'注册成功'); 
+                // $this->ret(1,$fan->extract($schema->columns(), true),'注册成功');
+               
+                $data    = $entity->extract($this->getSchema()->columns(), true);
+                $success = $this->Fans->_insert($entity, $data);
+                 $this->ret(5, [
+                    'checkRule' => $this->Fans->checkRules($fan, 'create'),
+                    'data' => $data,
+                    'success' => $success
+                 ], '注册成功');
                 if ($this->Fans->save($fan)) {
-                    $this->ret(0,$fan->id,'注册成功'); 
-                }else{
+                    $this->ret(0, $fan->id, '注册成功');
+                } else {
                     $msgs = [];
                     foreach ($fan->__debugInfo()['[errors]'] as $name => $error) {
                         $msgs[] = $name . ':' . implode(',', array_values($error));
                     }
-                    $this->ret(3,$fan,implode(';', $msgs)); 
+                    $this->ret(3, $fan, implode(';', $msgs));
                 }
-            }  
+            }
         }
     }
 
-
-    private function getSessionKey($jscode){
-        $http = new Client();
+    private function getSessionKey($jscode)
+    {
+        $http        = new Client();
         $jsonPayload = [
-            'appid' => $this->appid,
+            'appid'  => $this->appid,
             'secret' => $this->secret,
         ];
-        $url = 'https://sz.api.weixin.qq.com/sns/jscode2session?js_code='.$jscode.'&grant_type=authorization_code';
-        $response = $http->get($url,$jsonPayload,['type' => 'json']);
+        $url      = 'https://sz.api.weixin.qq.com/sns/jscode2session?js_code=' . $jscode . '&grant_type=authorization_code';
+        $response = $http->get($url, $jsonPayload, ['type' => 'json']);
         return $response;
-        die;                
+        die;
     }
     /**
      * Add method
