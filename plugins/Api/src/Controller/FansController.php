b5be179b1d1aff7443fca0e00ce2a1f25f074ded
yinyinghuang <934646546@qq.com>
2019-03-27 14:32:40 +0800

    hot search

--- plugins/Api/src/Controller/FansController.php
+++ plugins/Api/src/Controller/FansController.php
@@ -4,6 +4,8 @@ namespace Api\Controller;
 use Api\Controller\AppController;
 use Cake\Http\Client;
 use Cake\I18n\Time;
+use Cake\Cache\Cache;
+use Cake\Core\Configure;
 
 /**
  * Fans Controller
@@ -18,39 +20,39 @@ class FansController extends AppController
 
     public function login()
     {
+        $params = $this->request->getData();
+        //在缓存中查找用户信息
+        if(!empty($params['pkey'])){
+            $userInfo = json_decode(Cache::read($params['pkey']));
+        }
+        if(empty($userInfo)){
+            //在缓存中查找openid
+            if(!empty($params['pkey'])){
+                $openid  = json_decode(Cache::read('user.openid.'.$params['pkey']));
+            }else{
+                $openid  = $this->getOpenid();
+                $params['pkey'] = $this->setTokenId();
+                Cache::write('user.openid.'.$params['pkey'],$openid );
+            }
+            //数据库中获取用户信息
+            $userInfo = $this->getUserInfo($openid);
+            $userInfo['pkey'] = $params['pkey'];
+            Cache::write($params['pkey'],$userInfo ); 
+        }
+        $this->ret(0,['pkey' => $params['pkey']]);
+        
+    }
+    //获取openid
+    private function getOpenid()
+    {
         $code = $this->request->getData('code');
         empty($code) && $this->ret(1, '', '缺少code');
-
         $this->sessionKey = $this->getSessionKey($code);
         if (array_key_exists('errcode', $this->sessionKey->json)) {
             $this->ret(2, '', $this->sessionKey->json['errmsg']);
         } else {
-            $openid = $this->sessionKey->json['openid'];
-            $fan    = $this->Fans->find()->where(['openid' => $openid])->first();
-
-            if ($fan) {
-                $this->ret(0, $fan->id, '登陆成功');
-            } else {
-                $fan         = $this->Fans->newEntity();
-                $fan->openid = $openid;
-                $fan->sign_up =(new Time($row->created))->i18nFormat('yyyy-MM-dd H:i:s');
-                $params      = json_decode($this->request->getData('user_msg_str'), true);
-                $fan         = $this->Fans->patchEntity($fan, $params);
-                $schema      = $this->Fans->getSchema();
-                $data        = $fan->extract($this->Fans->getSchema()->columns(), true);
-                $fan         = $this->Fans->_insert($fan, $data);
-                $this->ret(0, $fan->id, '注册成功');
-                //以下方式保存数据，openid保存失败，原因未知
-                // if ($this->Fans->save($fan)) {
-                //     $this->ret(0, $fan->id, '注册成功');
-                // } else {
-                //     $msgs = [];
-                //     foreach ($fan->__debugInfo()['[errors]'] as $name => $error) {
-                //         $msgs[] = $name . ':' . implode(',', array_values($error));
-                //     }
-                //     $this->ret(3, $fan, implode(';', $msgs));
-                // }
-            }
+            $openid = $this->sessionKey->json['openid'];      
+            return $openid;      
         }
     }
 
@@ -66,7 +68,54 @@ class FansController extends AppController
         return $response;
         die;
     }
+    private function getUserInfo($openid)
+    {
+        $fan    = $this->Fans->find()->where(['openid' => $openid])->first();
 
+        if ($fan) return $fan;
+        $fan         = $this->Fans->newEntity();
+        $fan->openid = $openid;
+        $fan->sign_up =(new Time($row->created))->i18nFormat('yyyy-MM-dd H:i:s');
+        $params      = json_decode($this->request->getData('user_msg_str'), true);
+        $fan         = $this->Fans->patchEntity($fan, $params);
+        $schema      = $this->Fans->getSchema();
+        $data        = $fan->extract($this->Fans->getSchema()->columns(), true);
+        $fan         = $this->Fans->_insert($fan, $data);
+        
+        //以下方式保存数据，openid保存失败，原因未知
+        // if ($this->Fans->save($fan)) {
+        //     $this->ret(0, $fan->id, '注册成功');
+        // } else {
+        //     $msgs = [];
+        //     foreach ($fan->__debugInfo()['[errors]'] as $name => $error) {
+        //         $msgs[] = $name . ':' . implode(',', array_values($error));
+        //     }
+        //     $this->ret(3, $fan, implode(';', $msgs));
+        // }
+        return $fan;
+    }
+    private function setTokenId()
+    {
+        $token_salt = Configure::consume('Security.salt');
+        $timestamp = time();//时间戳
+        $nostr = self::setNostr();
+        $arr = ["{$timestamp}", $nostr, $token_salt];
+        sort($arr);
+
+        $public_token_id = sha1(implode($arr));
+        $real_token_id = md5($public_token_id . '@123' . $token_salt);
+
+        $ret['public_token_id'] = $public_token_id;
+        $ret['real_token_id'] = $real_token_id;
+
+        return $ret;
+    }    
+    //生成随机字符串
+    public static function setNostr($len = 12)
+    {
+        $nostr = mb_substr(md5(mb_substr(str_shuffle('0987YaTzxc23CvBVNbM456nSmkEjhA678guXoZqwpQrhRq24werWtyu2547iopsIdfgU76hPj'), 0, $len)), 0, $len); //随机字符串
+        return $nostr;
+    }
     public function merchantLists($fan_id)
     {
         if (empty($fan_id)) {
