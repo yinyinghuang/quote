62f7614d7445a512bd25b73b42fccc76531f0bbf
yinyinghuang <934646546@qq.com>
2019-03-27 15:29:44 +0800

    hot search

--- plugins/Api/src/Controller/CategoriesController.php
+++ plugins/Api/src/Controller/CategoriesController.php
@@ -177,7 +177,7 @@ class CategoriesController extends AppController
             $this->ret(1, null, 'category_id缺失');
         }
         $cateBrands = $this->redis->read('category.brand.'.$category_id);
-        if($cateBrands) $this->ret(0, $cateFilterAttrs, '加载成功');
+        if($cateBrands) $this->ret(0, $cateBrands, '加载成功');
         $cateBrands = $this->_getCategoryBrand($category_id);
         $this->redis->write('category.brand.'.$category_id,$cateBrands);
         $this->ret(0, $cateBrands, ['分类信息加载成功']);
