b321e8b5db743e66e7bab262ad5293ab8df05b14
yinyinghuang <934646546@qq.com>
2019-03-27 18:10:45 +0800

    hot search

--- plugins/Api/src/Controller/FansController.php
+++ plugins/Api/src/Controller/FansController.php
@@ -19,11 +19,101 @@ class FansController extends AppController
 
     public function login()
     {
-        $userInfo =$this->userInfo;
-        // $this->ret(2,$_POST);
-        $this->ret(3,$this->request->getData());
-        $this->ret(0,['pkey' => $userInfo['pkey']]);
+        $params = $this->request->getData();
+        //在缓存中查找用户信息
+        if(!empty($params['pkey'])){
+            $userInfo = json_decode($this->redis->read($params['pkey']));
+        }
+        if(empty($userInfo)){
+            //在缓存中查找openid
+            if(!empty($params['pkey'])){
+                $openid  = json_decode($this->redis->read('user.openid.'.$params['pkey']));
+            }else{
+                $openid  = $this->getOpenid();
+                $params['pkey'] = $this->setTokenId()['public_token_id'];
+                $this->redis->write('user.openid.'.$params['pkey'],$openid );
+            }
+            //数据库中获取用户信息
+            $userInfo = $this->getUserInfo($openid);
+            $userInfo['pkey'] = $params['pkey'];
+            $this->redis->write($params['pkey'],$userInfo ); 
+        }
+        $this->ret(0,['pkey' => $params['pkey']]);
         
+    }
+    //获取openid
+    private function getOpenid()
+    {
+        $code = $this->request->getData('code');
+        empty($code) && $this->ret(1, '', '缺少code');
+        $this->sessionKey = $this->getSessionKey($code);
+        if (array_key_exists('errcode', $this->sessionKey->json)) {
+            $this->ret(2, '', $this->sessionKey->json['errmsg']);
+        } else {
+            $openid = $this->sessionKey->json['openid'];      
+            return $openid;      
+        }
+    }
+
+    private function getSessionKey($jscode)
+    {
+        $http        = new Client();
+        $jsonPayload = [
+            'appid'  => $this->appid,
+            'secret' => $this->secret,
+        ];
+        $url      = 'https://sz.api.weixin.qq.com/sns/jscode2session?js_code=' . $jscode . '&grant_type=authorization_code';
+        $response = $http->get($url, $jsonPayload, ['type' => 'json']);
+        return $response;
+        die;
+    }
+    private function getUserInfo($openid)
+    {
+        $fan    = $this->Fans->find()->where(['openid' => $openid])->first();
+
+        if ($fan) return $fan;
+        $fan         = $this->Fans->newEntity();
+        $fan->openid = $openid;
+        $fan->sign_up =(new Time($row->created))->i18nFormat('yyyy-MM-dd H:i:s');
+        $params      = json_decode($this->request->getData('user_msg_str'), true);
+        $fan         = $this->Fans->patchEntity($fan, $params);
+        $schema      = $this->Fans->getSchema();
+        $data        = $fan->extract($this->Fans->getSchema()->columns(), true);
+        $fan         = $this->Fans->_insert($fan, $data);
+        
+        //以下方式保存数据，openid保存失败，原因未知
+        // if ($this->Fans->save($fan)) {
+        //     $this->ret(0, $fan->id, '注册成功');
+        // } else {
+        //     $msgs = [];
+        //     foreach ($fan->__debugInfo()['[errors]'] as $name => $error) {
+        //         $msgs[] = $name . ':' . implode(',', array_values($error));
+        //     }
+        //     $this->ret(3, $fan, implode(';', $msgs));
+        // }
+        return $fan;
+    }
+    private function setTokenId()
+    {
+        $token_salt = Configure::consume('Security.salt');
+        $timestamp = time();//时间戳
+        $nostr = self::setNostr();
+        $arr = ["{$timestamp}", $nostr, $token_salt];
+        sort($arr);
+
+        $public_token_id = sha1(implode($arr));
+        $real_token_id = md5($public_token_id . '@123' . $token_salt);
+
+        $ret['public_token_id'] = $public_token_id;
+        $ret['real_token_id'] = $real_token_id;
+
+        return $ret;
+    }    
+    //生成随机字符串
+    public static function setNostr($len = 12)
+    {
+        $nostr = mb_substr(md5(mb_substr(str_shuffle('0987YaTzxc23CvBVNbM456nSmkEjhA678guXoZqwpQrhRq24werWtyu2547iopsIdfgU76hPj'), 0, $len)), 0, $len); //随机字符串
+        return $nostr;
     }
     public function merchantLists($pkey)
     {
