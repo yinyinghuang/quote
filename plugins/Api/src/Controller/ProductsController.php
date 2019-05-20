29d4e387b13e6201dc03131078e3272338795245
yinyinghuang <934646546@qq.com>
2019-05-20 15:11:27 +0800

    update

--- plugins/Api/src/Controller/ProductsController.php
+++ plugins/Api/src/Controller/ProductsController.php
@@ -144,7 +144,7 @@ class ProductsController extends AppController
         ];
         $conditions = ['Quotes.is_visible' => 1, 'Quotes.product_id' => $product_id];
         $contain    = ['Merchants'];
-        $order      = ['Quotes.sort desc', 'Merchants.sort desc', 'Quotes.id desc', 'Merchants.id desc'];
+        $order      = ['Quotes.sort desc', 'Merchants.sort desc', 'Quotes.modified desc','Quotes.id desc', 'Merchants.id desc'];
         $limit      = 100;
         $offset     = $this->getOffset(isset($params['page']) ? $params['page'] : 1, $limit);
 
